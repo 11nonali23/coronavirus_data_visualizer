1c1185343281fdcdbc3c7a0b5d7f07cf91276c73
11nonali23 <11nonali23@gmail.com>
2020-03-12 19:38:47 +0100

    added bash script to automate update process, it will be ran dayly at 5, 6, 7 pm by another script

---
+++ site_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "script will update git repository"
+
+cd /home/andrea/Scaricati/ProtCivileDati/COVID-19
+
+git pull
+
+echo "done updating\n\n"
+
+echo "script will update html code"
+
+cd /home/andrea/Scrivania/Python/Data_Science/ncovid2019_tracker/
+
+python create_html.py
+
+echo "done updating"
+
+exec bash
