500bef4da722a822c6f05f610e0f59fb4419b9d6
11nonali23 <11nonali23@gmail.com>
2020-03-13 21:41:34 +0100

    updated bash script

--- site_update.sh
+++ site_update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "script will update git repository"
+#this script will run daily at 6,7,8 pm
+
+echo "script will update Protezione civile git repository"
 
 cd /home/andrea/Scaricati/ProtCivileDati/COVID-19
 
@@ -15,6 +17,15 @@ cd /home/andrea/Scrivania/Python/Data_Science/ncovid2019_tracker/
 python create_html.py
 
 echo "done updating"
+
+echo "adding files to git"
+
+git add .
+
+git commit -m "added new data"
+
+git push
+
 
 exit
 
