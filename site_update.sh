3b2e72e41fae42cfa456888de58c46e90b3816e5
Andrea <11nonali23@gmail.com>
2020-03-22 21:47:54 +0100

    prettified script

--- site_update.sh
+++ site_update.sh
@@ -2,31 +2,25 @@
 
 #this script will run daily at 6,7,8 pm
 
+#getting into protezione civile and pull data
 echo "script will update Protezione civile git repository"
-
-cd /home/andrea/Scaricati/ProtCivileDati/COVID-19
-
+cd /home/andrean/Scaricati/ProtCivileDati/COVID-19/
 git pull
-
 echo "done updating"
 
+#getting into project dir and update html code with python script
 echo "script will update html code"
-
-cd /home/andrea/Scrivania/Python/Data_Science/ncovid2019_tracker/
-
+cd /home/andrean/Scrivania/ncovid2019_data_visualizer/
 python create_html.py
-
 echo "done updating"
 
+#adding files to remote git repo
 echo "adding files to git"
-
 git add .
-
 git commit -m "added new data"
-
 git push
-
 
 exit
 
+#saying script to execute in the bash is launched
 exec bash
